156c7e762dddb8d340fbd468a4ec931cf3b26cb8
Administrador del Dominio <Administrador@vialidad.gba.gov.ar>
2025-07-10 09:47:01 -0300

    server express

---
+++ README.md
@@ -0,0 +1,7 @@
+# API REST con Node.js y Fierebase
+
+#instalacion
+
+```shell
+npm install 
+```
---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+const app = express();
+
+
+
+
+
+app.get("/",(req,res)=>{
+    res.send('Bienvenido a nuestra API REST');
+});
+
+
+
+const PORT =3000;
+
+
+app.listen(PORT,()=> console.log(`http://localhost:${PORT}`));
